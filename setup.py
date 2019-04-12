f3bf5580e72c847e1e63e755f19c57889cb72acf
Andrew Poltavchenko <pa@yourserveradmin.com>
2019-04-13 00:01:41 +0300

    Alphabetically sort setup entries in setup.py

--- setup.py
+++ setup.py
@@ -8,26 +8,14 @@ with open('README.md', 'r') as fp:
     long_description = fp.read()
 
 setup(
-    name='toggl2pl',
-    version=version,
-    packages=[
-        'toggl2pl'
-    ],
-    scripts=[
-        'scripts/toggl2pl'
-    ],
-    url='https://git-y.yourserveradmin.com/pa/toggl2pl',
-    license='MIT',
+    author='Andrew Poltavchenko',
+    author_email='pa@yourserveradmin.com',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
-    author='Andrew Poltavchenko',
-    author_email='pa@yourserveradmin.com',
     description='Python module and tool to simplify time entries export from Toggl into Project Laboratory',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts':
             [
@@ -40,5 +28,17 @@ setup(
         'tabulate',
         'tqdm',
         'yaml'
-    ]
+    ],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='toggl2pl',
+    packages=[
+        'toggl2pl'
+    ],
+    scripts=[
+        'scripts/toggl2pl'
+    ],
+    url='https://git-y.yourserveradmin.com/pa/toggl2pl',
+    version=version,
 )
