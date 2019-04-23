8ee700f70f7433aa916c9edb3546d6e9209a1851
Andrew Poltavchenko <pa@yourserveradmin.com>
2019-04-23 18:10:13 +0300

    Implement posts/push route to post tasks into PL

--- toggl2pl/__serve__.py
+++ toggl2pl/__serve__.py
@@ -63,4 +63,42 @@ def pull():
 
 @posts.route(rule='/push', methods=['PUT'])
 def push():
-    pass
+    """
+    Push single Project Laboratory task information.
+
+    .. :quickref: Pull Posts; Pull posts from Toggl and send to client.
+
+    :reqheader Content-Type: application/json
+
+    :<json string api_token: The Toggl authentication token to use instead of username and password.
+    :<json string date: The date in ISO 8601 (`YYYY-MM-DD`) format when work was actually done.
+    :<json string description: Relatively short description of the work done as a part of the parent task.
+    :<json integer minutes: Total amount of minutes spent during work on the task entry.
+    :<json string project: The Project Laboratory task parent project name.
+    :<json string task: The Project Laboratory task name.
+    :<json string user_key: The Project Laboratory authentication token to use instead of username and password.
+    :<json string workspace: The Toggl workspace name (case sensitive) to pull information from.
+
+    :resheader Content-Type: application/json
+
+    :status 200: Request successfully processed and response provided back to client.
+    """
+    data = request.get_json()
+    client = Client(
+        api_token=data['api_token'],
+        base_url=settings['base_url'],
+        excluded_projects=None,
+        log_level=settings['log_level'],
+        user_key=data['user_key'],
+        verify=settings['verify'],
+        workspace=data['workspace'],
+    )
+    return jsonify(
+        client.add_post(
+            date=data['date'],
+            description=data['description'],
+            minutes=data['minutes'],
+            project=data['project'],
+            task=data['task']
+        )
+    )
